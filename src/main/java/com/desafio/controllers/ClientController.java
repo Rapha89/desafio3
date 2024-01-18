99a2d64a13d1b12833f3fd09af52e6fbe061b37f
Raphael Vacari <raphaelvacari7@gmail.com>
2024-01-18 17:35:15 -0300

    Criando as classes restantes

---
+++ src/main/java/com/desafio/controllers/ClientController.java
@@ -0,0 +1,18 @@
+package com.desafio.controllers;
+
+import com.desafio.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientController {
+
+    @Autowired
+    private ClientService service;
+
+
+
+
+}
