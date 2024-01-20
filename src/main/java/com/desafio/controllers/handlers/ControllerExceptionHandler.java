312c2dc40cbab6ed89a186f1e1f5b171dd42dd30
Raphael Vacari <raphaelvacari7@gmail.com>
2024-01-20 14:24:25 -0300

    Busca por id retorna 404 para cliente inexistente

---
+++ src/main/java/com/desafio/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.desafio.controllers.handlers;
+
+import com.desafio.dto.CustomError;
+import com.desafio.services.exceptions.ClientNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ClientNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ClientNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+}
